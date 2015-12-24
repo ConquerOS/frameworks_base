5198bc89d0a3278e29656f1dc637d96f755f5037
Alex Cruz <mazdarider23@gmail.com>
2015-12-24 20:34:11 +0000

    SystemUI: Show 4G instead of LTE switch [1/2]
    jhenrique09 edits: Code cleanup
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Change-Id: I95e605c6b6774abcfd5d77a8c59d94b3f5593d88
    
    spezi77: Updated to androidx
    Change-Id: I41c9976f1c8e8c1ff94f8003aa590fa6eee85b7b
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: HeroBuxx <me@herobuxx.xyz>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4932,6 +4932,12 @@ public final class Settings {
        public static final String GLOBAL_ACTIONS_MAX_COLUMNS = "global_actions_max_columns";
 
         /**
+         * Show 4G instead of LTE
+         * @hide
+         */
+        public static final String SHOW_FOURG = "show_fourg";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -5057,6 +5063,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(OMNI_QS_LAYOUT_ROWS);
             PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_GESTURE);
             PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_LOCKSCREEN);
+            PRIVATE_SETTINGS.add(SHOW_FOURG);
         }
 
         /**
