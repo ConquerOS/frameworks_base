df6ff6c90dc182a44121031fd3c761c105395c36
beanstown106 <nbenis106@gmail.com>
2016-01-14 05:56:50 -0500

    Fingerprint authentication vibration [1/2]
    had a few requests for this, as most devices only vibrate when fingerprint authentication is unsucesful so made it configurable
    
    xyyx: adapted to 8.1
    
    DennySPb: add on_error switch
    
    Change-Id: Iad25150d8d322d69e142d45f552917777003a0e2
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: HeroBuxx <me@herobuxx.xyz>
    Signed-off-by: HeroBuxx <herobuxx@conqueros.co>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4938,6 +4938,20 @@ public final class Settings {
         public static final String SHOW_FOURG = "show_fourg";
 
         /**
+         * whether to enable or disable vibration on succesful fingerprint auth
+         *
+         * @hide
+         */
+        public static final String FINGERPRINT_SUCCESS_VIB = "fingerprint_success_vib";
+
+        /**
+         * whether to enable or disable vibration on error fingerprint auth
+         *
+         * @hide
+         */
+        public static final String FINGERPRINT_ERROR_VIB = "fingerprint_error_vib";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
