e028c56c6a9583c74ed2648044146ab11b37ce46
maxwen <max.weninger@gmail.com>
2017-02-24 01:28:00 +0300

    Hide Statusbar on Lockscreen [1/2]
    Taken from Omni ROM FWB-
    https://github.com/omnirom/android_frameworks_base/commit/0b833a0fc797d21c632ff1e49c64d6ebff97d90d
    
    Source - https://github.com/omnirom/android_frameworks_base/commit/0b833a0fc797d21c632ff1e49c64d6ebff97d90d
    
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>
    Change-Id: I378ea1a8e7c196026e551aedb23e44c59e26f55a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4679,6 +4679,15 @@ public final class Settings {
         public static final String STATUS_BAR_CLOCK_FONT_STYLE = "status_bar_clock_font_style";
 
         /**
+        /**
+         * @hide
+         */
+        public static final String LOCK_SHOW_STATUS_BAR = "lockscreen_show_status_bar";
+
+        /** @hide */
+        private static final Validator LOCK_SHOW_STATUS_BAR_VALIDATOR = BOOLEAN_VALIDATOR;
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4755,6 +4764,7 @@ public final class Settings {
             STATUSBAR_CLOCK_DATE_STYLE,
             STATUSBAR_CLOCK_DATE_FORMAT,
             STATUSBAR_CLOCK_DATE_POSITION,
+            LOCK_SHOW_STATUS_BAR,
         };
 
         /**
@@ -4887,6 +4897,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(STATUSBAR_CLOCK_DATE_STYLE);
             PRIVATE_SETTINGS.add(STATUSBAR_CLOCK_DATE_FORMAT);
             PRIVATE_SETTINGS.add(STATUSBAR_CLOCK_DATE_POSITION);
+            PRIVATE_SETTINGS.add(LOCK_SHOW_STATUS_BAR);
         }
 
         /**
@@ -4993,6 +5004,7 @@ public final class Settings {
             VALIDATORS.put(STATUSBAR_CLOCK_DATE_STYLE, STATUSBAR_CLOCK_DATE_STYLE_VALIDATOR);
             VALIDATORS.put(STATUSBAR_CLOCK_DATE_FORMAT, STATUSBAR_CLOCK_DATE_FORMAT_VALIDATOR);
             VALIDATORS.put(STATUSBAR_CLOCK_DATE_POSITION, STATUSBAR_CLOCK_DATE_POSITION_VALIDATOR);
+            VALIDATORS.put(LOCK_SHOW_STATUS_BAR,LOCK_SHOW_STATUS_BAR_VALIDATOR);
         }
 
         /**
