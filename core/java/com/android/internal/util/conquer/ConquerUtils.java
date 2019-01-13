d14edf67b1caad3e56f0d513a9589e60c526656a
Alex Cruz <du.alexcruz@gmail.com>
2019-01-13 11:10:54 -0600

    ConquerUtils: Method to detect a notch'd device
    Change-Id: I4742eaeea94c4495361bdfe2b855ae0de0aa24c1
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>

--- core/java/com/android/internal/util/conquer/ConquerUtils.java
+++ core/java/com/android/internal/util/conquer/ConquerUtils.java
@@ -205,4 +205,23 @@ public class ConquerUtils {
             pm.goToSleep(SystemClock.uptimeMillis());
         }
     }
+
+    // Check if device has a notch
+    public static boolean hasNotch(Context context) {
+        int result = 0;
+        int resid;
+        int resourceId = context.getResources().getIdentifier(
+                "status_bar_height", "dimen", "android");
+        resid = context.getResources().getIdentifier("config_fillMainBuiltInDisplayCutout",
+                "bool", "android");
+        if (resid > 0) {
+            return context.getResources().getBoolean(resid);
+        }
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+        float px = 24 * (metrics.densityDpi / 160f);
+        return result > Math.round(px);
+    }
 }
