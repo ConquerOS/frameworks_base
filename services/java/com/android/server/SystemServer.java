8a6cc8690e7fd5588e7aceb837c8571569910b4a
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-05-03 16:49:15 -0300

    Introduce LiveDisplay from Lineage
    All credits to LineageOS and related authors on:
    
    https://github.com/LineageOS/android_lineage-sdk
    
    Included fwb commit:
    
    commit 5ef7a9bb0996cc0c141d054514744ceaa93ba036
    Author: Dan Pasanen <dan.pasanen@gmail.com>
    Date:   Tue Feb 20 07:51:43 2018 -0600
    
        NightDisplayController: report unavailable if livedisplay feature is present
    
        Change-Id: Id677e76d452ced2e62373337d63593c7b85f2fba
    
    Syberia: forward port to 10, 11
    
    squashed with:
    lineage-sdk: ServiceType.GPS -> ServiceType.LOCATION
    lineage-sdk: Update DisplayTransformManager class path
    
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Change-Id: I35bf801aca2e31f30e9d1a0488648f93eab84492
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: HeroBuxx <herobuxx@conqueros.co>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -197,6 +197,11 @@ import java.util.Timer;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Future;
 
+
+// LiveDisplay
+import com.android.server.custom.LineageHardwareService;
+import com.android.server.custom.display.LiveDisplayService;
+
 public final class SystemServer {
 
     private static final String TAG = "SystemServer";
@@ -2051,6 +2056,14 @@ public final class SystemServer {
 
             t.traceBegin("StartPeopleService");
             mSystemServiceManager.startService(PeopleService.class);
+            t.traceEnd();
+
+            // LiveDisplay
+            t.traceBegin("StartLineageHardwareService");
+            mSystemServiceManager.startService(LineageHardwareService.class);
+            t.traceEnd();
+            t.traceBegin("StartLiveDisplayService");
+            mSystemServiceManager.startService(LiveDisplayService.class);
             t.traceEnd();
         }
 
