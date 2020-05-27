d7fb358e109322c37243587fd6ea32e065f0b22d
Ashwin R C <ashwin2001achu@gmail.com>
2020-05-27 10:26:16 +0000

    SystemUI: Adapt screenshot sound to ringer modes
    @xyzuan
    Adapted for Android 13
    
    Co-authored-by: xyzuan <xyzuannihboss@gmail.com>
    Co-authored-by: althafvly <althafvly@gmail.com>
    Change-Id: I381c351131241e45ddb6049706d6c302c2eee946
    Signed-off-by: HeroBuxx <me@buxxed.tk>

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -55,10 +55,13 @@ import android.hardware.display.DisplayManager;
 import android.media.AudioAttributes;
 import android.media.AudioSystem;
 import android.media.MediaPlayer;
+import android.media.AudioManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.provider.Settings;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -256,12 +259,14 @@ public class ScreenshotController {
     private final WindowManager mWindowManager;
     private final WindowManager.LayoutParams mWindowLayoutParams;
     private final AccessibilityManager mAccessibilityManager;
+    private final AudioManager mAudioManager;
     private final ListenableFuture<MediaPlayer> mCameraSound;
     private final ScrollCaptureClient mScrollCaptureClient;
     private final PhoneWindow mWindow;
     private final DisplayManager mDisplayManager;
     private final ScrollCaptureController mScrollCaptureController;
     private final LongScreenshotData mLongScreenshotHolder;
+    private final Vibrator mVibrator;
     private final boolean mIsLowRamDevice;
     private final TimeoutHandler mScreenshotHandler;
 
@@ -341,6 +346,10 @@ public class ScreenshotController {
 
         // Setup the Camera shutter sound
         mCameraSound = loadCameraSound();
+
+        // Grab system services needed for screenshot sound
+        mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+        mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
 
         mCopyBroadcastReceiver = new BroadcastReceiver() {
             @Override
@@ -863,14 +872,30 @@ public class ScreenshotController {
         }, mBgExecutor);
     }
 
+    private void playShutterSoundIf() {
+        switch (mAudioManager.getRingerMode()) {
+            case AudioManager.RINGER_MODE_SILENT:
+                // do nothing
+                break;
+            case AudioManager.RINGER_MODE_VIBRATE:
+                if (mVibrator != null && mVibrator.hasVibrator()) {
+                    mVibrator.vibrate(VibrationEffect.createOneShot(50,
+                            VibrationEffect.DEFAULT_AMPLITUDE));
+                }
+                break;
+            case AudioManager.RINGER_MODE_NORMAL:
+                // Play the shutter sound to notify that we've taken a screenshot
+                playCameraSound();
+                break;
+        }
+    }
+
     /**
      * Save the bitmap but don't show the normal screenshot UI.. just a toast (or notification on
      * failure).
      */
     private void saveScreenshotAndToast(Consumer<Uri> finisher) {
-        // Play the shutter sound to notify that we've taken a screenshot
-        playCameraSound();
-
+        playShutterSoundIf();
         saveScreenshotInWorkerThread(
                 /* onComplete */ finisher,
                 /* actionsReadyListener */ imageData -> {
@@ -903,7 +928,7 @@ public class ScreenshotController {
                 mScreenshotView.createScreenshotDropInAnimation(screenRect, showFlash);
 
         // Play the shutter sound to notify that we've taken a screenshot
-        playCameraSound();
+        playShutterSoundIf();
 
         if (DEBUG_ANIM) {
             Log.d(TAG, "starting post-screenshot animation");
