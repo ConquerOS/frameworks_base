b5f243d409539be9c030788e72b5cf2b501d8f90
Alex Cruz <du.alexcruz@gmail.com>
2020-05-28 06:24:40 +0000

    Lockscreen Clock Widget Font Style [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4772,6 +4772,12 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
 
         /**
+         * Change font style for the system lockscreen clock widget
+         * @hide
+         */
+        public static final String LOCK_CLOCK_FONT_STYLE = "lock_clock_font_style";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -21,6 +21,7 @@ import android.app.IActivityManager;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.RemoteException;
@@ -42,6 +43,7 @@ import com.android.internal.widget.LockPatternUtils;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
 import com.android.systemui.statusbar.policy.ConfigurationController;
+import android.provider.Settings;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -83,6 +85,7 @@ public class KeyguardStatusView extends GridLayout implements
         @Override
         public void onTimeChanged() {
             refreshTime();
+            refreshLockFont();
         }
 
         @Override
@@ -144,6 +147,11 @@ public class KeyguardStatusView extends GridLayout implements
      */
     public boolean hasCustomClock() {
         return mClockView.hasCustomClock();
+    }
+
+    private int getLockClockFont() {
+        return Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.LOCK_CLOCK_FONT_STYLE, 0);
     }
 
     /**
@@ -239,6 +247,7 @@ public class KeyguardStatusView extends GridLayout implements
         if (mClockView != null) {
             mClockView.setTextSize(TypedValue.COMPLEX_UNIT_PX,
                     getResources().getDimensionPixelSize(R.dimen.widget_big_font_size));
+            refreshLockFont();
         }
         if (mOwnerInfo != null) {
             mOwnerInfo.setTextSize(TypedValue.COMPLEX_UNIT_PX,
@@ -329,6 +338,60 @@ public class KeyguardStatusView extends GridLayout implements
     @Override
     public void onLocaleListChanged() {
         refreshFormat();
+    }
+
+    private void refreshLockFont() {
+		setFontStyle(mClockView, getLockClockFont());
+    }
+
+    private void setFontStyle(KeyguardClockSwitch view, int fontstyle) {
+    	if (view != null) {
+    		switch (fontstyle) {
+    			case 0:
+    			default:
+    				view.setTextFont(Typeface.create(mContext.getResources().getString(R.string.clock_sysfont_headline_medium), Typeface.NORMAL));
+    				break;
+    			case 1:
+    				view.setTextFont(Typeface.create(mContext.getResources().getString(R.string.clock_sysfont_body_medium), Typeface.NORMAL));
+    				break;
+    			case 2:
+    				view.setTextFont(Typeface.create("sans-serif", Typeface.BOLD));
+    				break;
+    			case 3:
+    				view.setTextFont(Typeface.create("sans-serif", Typeface.NORMAL));
+    				break;
+    			case 4:
+    				view.setTextFont(Typeface.create("sans-serif", Typeface.ITALIC));
+    				break;
+    			case 5:
+    				view.setTextFont(Typeface.create("sans-serif", Typeface.BOLD_ITALIC));
+    				break;
+    			case 6:
+    				view.setTextFont(Typeface.create("sans-serif-light", Typeface.NORMAL));
+    				break;
+    			case 7:
+    				view.setTextFont(Typeface.create("sans-serif-thin", Typeface.NORMAL));
+    				break;
+    			case 8:
+    				view.setTextFont(Typeface.create("sans-serif-condensed", Typeface.NORMAL));
+    				break;
+    			case 9:
+    				view.setTextFont(Typeface.create("sans-serif-condensed", Typeface.ITALIC));
+    				break;
+    			case 10:
+    				view.setTextFont(Typeface.create("sans-serif-condensed", Typeface.BOLD));
+    				break;
+    			case 11:
+    				view.setTextFont(Typeface.create("sans-serif-condensed", Typeface.BOLD_ITALIC));
+    				break;
+    			case 12:
+    				view.setTextFont(Typeface.create("sans-serif-medium", Typeface.NORMAL));
+    				break;
+    			case 13:
+    				view.setTextFont(Typeface.create("sans-serif-medium", Typeface.ITALIC));
+    				break;
+    		}
+    	}
     }
 
     public void dump(FileDescriptor fd, PrintWriter pw, String[] args) {
