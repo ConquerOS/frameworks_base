64be5e7c8e1400c898ef4defc71235d5295af602
rohan <purohit.rohan@gmail.com>
2020-07-14 13:35:24 -0400

    SystemUI: Add OnePlus Roman analog clock
    * Inspired by oneplus aod clocks
      thanks to @ROGERdotT for the design.
    
    Change-Id: I50872ae8b572d4b24607f9f30d1fbde5c6b609a5
    Signed-off-by: MLZ94 <zugutoroberto@gmail.com>
    Signed-off-by: HeroBuxx <herobuxx@conqueros.co>

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -157,6 +157,7 @@ public final class ClockManager {
         addBuiltinClock(() -> new IDEClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new ShapeShiftClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new SfunyClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new OPRomanClockController(res, layoutInflater, colorExtractor));
 
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
