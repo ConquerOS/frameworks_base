c2568e01d966c34b15bf4e081ca30b6745bd219a
Pig <pig.priv@gmail.com>
2020-12-11 04:01:10 +0800

    fixup! FODCircleView: various improvements.
     * Make sure fod pressed icon is hidden and showCircle call is ignored as well.
    
    Change-Id: I289a9e5a536a3830e0e50393a8aeb6da003413bb
    Signed-off-by: HeroBuxx <herobuxx@conqueros.co>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -99,7 +99,26 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
                 return;
             }
 
+        if (!mUpdateMonitor.isScreenOn()) {
+                // Keyguard is shown just after screen turning off
+                return;
+            }
+
+            if (mIsBouncer && !isPinOrPattern(mUpdateMonitor.getCurrentUser())) {
+                // Ignore show calls when Keyguard password screen is being shown
+                return;
+            }
+
+            if (mIsKeyguard && mUpdateMonitor.getUserCanSkipBouncer(mUpdateMonitor.getCurrentUser())) {
+                // Ignore show calls if user can skip bouncer
+                return;
+            }
+
+            if (mIsKeyguard && !mIsBiometricRunning) {
+                return;
+            }
             mHandler.post(() -> showCircle());
+
         }
 
         @Override
@@ -263,7 +282,6 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
                 super.onDraw(canvas);
             }
         };
-        mPressedView.setImageResource(R.drawable.fod_icon_pressed);
 
         mWindowManager.addView(this, mParams);
 
@@ -380,6 +398,8 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
 
         setDim(true);
         dispatchPress();
+
+        mPressedView.setImageResource(R.drawable.fod_icon_pressed);
 
         setImageDrawable(null);
         invalidate();
