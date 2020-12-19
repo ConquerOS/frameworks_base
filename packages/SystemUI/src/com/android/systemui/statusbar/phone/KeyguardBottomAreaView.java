a7ca8d70cf2454e1998bc852d0610b62e3842f26
Jake Weinstein <jake@aospa.co>
2020-12-19 19:20:46 +0100

    SystemUI: Squashed Revert of CAF Emergency Button additions
    - Matches AOSP UX
    - Avoids conflict with FOD UI
    
    Revert "SystemUI: show emergency button on lock screen"
    
    This reverts commit d19f1655992a3660bd8a6786f47e5a1ec57aee53.
    
    Revert "SystemUI: Fix two emergency button issue on keyguard"
    
    This reverts commit b67bcb7e57731a0ead222d57dc9ceb5b1915d0a5.
    
    Change-Id: Iec5ce76bc20e8168b24f3eda9c71ebceb9614a4d
    
    Revert "SystemUI: Fix an issue where the EMERGENCY button clicks no response"
    
    This reverts commit c10ff2fb79be3153209b895e9a20f2978629e4d9.
    
    Change-Id: Id7fc682dd138dcf052925f54f173cedacce41efd
    
    Revert "SystemUI: Change for Emergency Button"
    
    This reverts commit 45e37e29b9b1a45a095df1299b23590aa314f370.
    
    Change-Id: I571883251f5f475361040c21cdfc463adf7ef61a
    Signed-off-by: HeroBuxx <herobuxx@conqueros.co>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -67,7 +67,6 @@ import android.widget.TextView;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.util.custom.fod.FodUtils;
 import com.android.internal.widget.LockPatternUtils;
-import com.android.keyguard.EmergencyCarrierArea;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.KeyguardUpdateMonitorCallback;
 import com.android.systemui.ActivityIntentHelper;
@@ -122,8 +121,6 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
     protected static final Intent PHONE_INTENT = new Intent(Intent.ACTION_DIAL);
     private static final int DOZE_ANIMATION_STAGGER_DELAY = 48;
     private static final int DOZE_ANIMATION_ELEMENT_DURATION = 250;
-
-    private EmergencyCarrierArea mEmergencyCarrierArea;
 
     private final boolean mShowLeftAffordance;
     private final boolean mShowCameraAffordance;
@@ -243,7 +240,6 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
         mPreviewInflater = new PreviewInflater(mContext, new LockPatternUtils(mContext),
                 new ActivityIntentHelper(mContext));
         mPreviewContainer = findViewById(R.id.preview_container);
-        mEmergencyCarrierArea = (EmergencyCarrierArea) findViewById(R.id.keyguard_selector_fade_container);
         mOverlayContainer = findViewById(R.id.overlay_container);
         mRightAffordanceView = findViewById(R.id.camera_button);
         mLeftAffordanceView = findViewById(R.id.left_button);
@@ -818,10 +814,8 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
 
         if (dozing) {
             mOverlayContainer.setVisibility(INVISIBLE);
-            mEmergencyCarrierArea.setVisibility(INVISIBLE);
         } else {
             mOverlayContainer.setVisibility(VISIBLE);
-            mEmergencyCarrierArea.setVisibility(VISIBLE);
             if (animate) {
                 startFinishDozeAnimation();
             }
@@ -849,7 +843,6 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
         mLeftAffordanceView.setAlpha(alpha);
         mRightAffordanceView.setAlpha(alpha);
         mIndicationArea.setAlpha(alpha);
-        mEmergencyCarrierArea.setAlpha(alpha);
     }
 
     private class DefaultLeftButton implements IntentButton {
