2288dd98cdabee02ea36e8d4cf4526d62b3608e3
TikkiTikki <radstylemusic@gmail.com>
2021-02-03 21:43:34 -0300

    base: Replace media notification background with artwork [1/2]
    Why not?
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: HeroBuxx <herobuxx@conqueros.co>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5099,6 +5099,12 @@ public final class Settings {
         public static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL = "lockscreen_quick_unlock_control";
 
         /**
+         * Enable and disable Artwork on background media notification
+         * @hide
+         */
+        public static final String ARTWORK_MEDIA_BACKGROUND = "artwork_media_background";
+
+        /**
          * These are all public system settings
          *
          * @hide
