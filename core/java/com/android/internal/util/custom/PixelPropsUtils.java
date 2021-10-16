ab9f8b52e45aa51a58fee9363ac0a56ae2ea7523
jhenrique09 <jhsv09@gmail.com>
2021-10-16 12:18:09 -0300

    PixelPropsUtils: Set userdebug as Build.TYPE for GMS
    Fixes force close on google apps
    
    10-15 07:44:52.294  8041  8252 E AndroidRuntime: FATAL EXCEPTION: DG
    10-15 07:44:52.294  8041  8252 E AndroidRuntime: Process: com.google.android.youtube, PID: 8041
    10-15 07:44:52.294  8041  8252 E AndroidRuntime: java.lang.SecurityException: GoogleCertificatesRslt: Package signed with debug key (go/gsrlt)
    
    Change-Id: Idb11a2f032ebe6d162363b0a644ad7ab6d37393a
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -76,6 +76,7 @@ public class PixelPropsUtils {
         }
         if (packageName.equals(PACKAGE_GMS)) {
             sIsGms = true;
+            setPropValue("TYPE", "userdebug");
         }
         if (packageName.startsWith("com.google.") || Arrays.asList(extraPackagesToChange).contains(packageName)) {
             Map<String, Object> propsToChange = propsToChangePixel6;
