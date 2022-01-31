P-404/android_frameworks_base

SystemUI: integrate smartspace
404: Smartspace jar downgraded to Nov one.

Change-Id: I6cf1e5147b502a0596e9cb2cc88eaefbd9487b50
Signed-off-by: Akash Srivastava <akashniki@gmail.com>

packages/SystemUI/src/com/p404/android/systemui/smartspace/KeyguardSmartspaceController.kt
@@ -1,0 +1,29 @@
+package com.p404.android.systemui.smartspace
+
+import android.content.ComponentName
+import android.content.Context
+
+import com.android.systemui.dagger.SysUISingleton
+import com.android.systemui.statusbar.FeatureFlags
+
+import com.google.android.systemui.smartspace.KeyguardMediaViewController
+import com.google.android.systemui.smartspace.KeyguardZenAlarmViewController
+
+import javax.inject.Inject
+
+@SysUISingleton
+class KeyguardSmartspaceController @Inject constructor(
+    private val context: Context,
+    private val featureFlags: FeatureFlags,
+    private val zenController: KeyguardZenAlarmViewController,
+    private val mediaController: KeyguardMediaViewController,
+) {
+    init {
+        if (!featureFlags.isSmartspaceEnabled()) {
+            context.packageManager.setComponentEnabledSetting(ComponentName("com.android.systemui", "com.p404.android.systemui.keyguard.KeyguardSliceProviderP404"), 1, 1)
+        } else {
+            mediaController.init()
+            zenController.init()
+        }
+    }
+}

ConquerOS/frameworks_base

packages/SystemUI/src/com/conquer/android/systemui/smartspace/KeyguardSmartspaceController.kt
@@ -1,0 +1,29 @@
+package com.conquer.android.systemui.smartspace
+
+import android.content.ComponentName
+import android.content.Context
+
+import com.android.systemui.dagger.SysUISingleton
+import com.android.systemui.statusbar.FeatureFlags
+
+import com.google.android.systemui.smartspace.KeyguardMediaViewController
+import com.google.android.systemui.smartspace.KeyguardZenAlarmViewController
+
+import javax.inject.Inject
+
+@SysUISingleton
+class KeyguardSmartspaceController @Inject constructor(
+    private val context: Context,
+    private val featureFlags: FeatureFlags,
+    private val zenController: KeyguardZenAlarmViewController,
+    private val mediaController: KeyguardMediaViewController,
+) {
+    init {
+        if (!featureFlags.isSmartspaceEnabled()) {
+            context.packageManager.setComponentEnabledSetting(ComponentName("com.android.systemui", "com.conquer.android.systemui.keyguard.KeyguardSliceProviderConquer"), 1, 1)
+        } else {
+            mediaController.init()
+            zenController.init()
+        }
+    }
+}