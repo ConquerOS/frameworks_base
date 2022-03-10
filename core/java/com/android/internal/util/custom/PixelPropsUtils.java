9df18c7909e9f6af73b2fd49116a89e7bb7a112d
Pulkit077 <pulkitagarwal2k1@gmail.com>
2022-03-10 09:17:15 +0530

    PixelPropsUtils: Update fingerprint to Oriole & Redfin Feb release

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -77,14 +77,14 @@ public class PixelPropsUtils {
         propsToChangePixel6.put("DEVICE", "oriole");
         propsToChangePixel6.put("PRODUCT", "raven");
         propsToChangePixel6.put("MODEL", "Pixel 6");
-        propsToChangePixel6.put("FINGERPRINT", "google/oriole/oriole:12/SQ1D.211205.016.A1/7957957:user/release-keys");
+        propsToChangePixel6.put("FINGERPRINT", "google/oriole/oriole:12/SQ1D.220205.004/8151327:user/release-keys");
         propsToChangePixel5 = new HashMap<>();
         propsToChangePixel5.put("BRAND", "google");
         propsToChangePixel5.put("MANUFACTURER", "Google");
         propsToChangePixel5.put("DEVICE", "redfin");
         propsToChangePixel5.put("PRODUCT", "redfin");
         propsToChangePixel5.put("MODEL", "Pixel 5");
-        propsToChangePixel5.put("FINGERPRINT", "google/redfin/redfin:12/SQ1A.211205.008/7888514:user/release-keys");
+        propsToChangePixel5.put("FINGERPRINT", "google/redfin/redfin:12/SQ1A.220205.002/8010174:user/release-keys");
         propsToChangePixelXL = new HashMap<>();
         propsToChangePixelXL.put("BRAND", "google");
         propsToChangePixelXL.put("MANUFACTURER", "Google");
